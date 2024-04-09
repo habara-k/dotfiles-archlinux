97c9a97149836afd04d91da066431008baac2ec5
Keigo Habara <k.habara.aa@gmail.com>
2024-04-10 00:04:07 +0900

    Update wezterm config: always open window in full-screen

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -1,4 +1,12 @@
 local wezterm = require("wezterm")
+
+local mux = wezterm.mux
+
+wezterm.on("gui-startup", function()
+	local _, _, window = mux.spawn_window({})
+	window:gui_window():maximize()
+end)
+
 local act = wezterm.action
 
 return {
