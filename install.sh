cac43d14d0ef41e9a4e216b715c462e4088f1406
Keigo Habara <k.habara.aa@gmail.com>
2024-04-11 21:17:41 +0900

    Disable aws status

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 ln -snfv $PWD/zshrc $HOME/.zshrc
 ln -snfv $PWD/zshenv $HOME/.zshenv
+ln -snfv $PWD/config/starship.toml $HOME/.config/starship.toml
 ln -snfv $PWD/config/nvim $HOME/.config/nvim
 ln -snfv $PWD/config/wezterm $HOME/.config/wezterm
 ln -snfv $PWD/config/sheldon $HOME/.config
